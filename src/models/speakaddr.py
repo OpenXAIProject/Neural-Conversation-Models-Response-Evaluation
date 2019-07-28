8c25b3827a85bd18ca215412bbb76d8a1b8343d9
NoSyu <dongdm@gmail.com>
2019-07-28 23:04:32 +0900

    Fix the bug

--- src/models/speakaddr.py
+++ src/models/speakaddr.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import layers
+from utils import to_var, pad
 
 
 class SpeakAddr(nn.Module):
@@ -25,31 +26,38 @@ class SpeakAddr(nn.Module):
         if config.tie_embedding:
             self.decoder.embedding = self.encoder.embedding
 
-    def forward(self, input_sentences, input_users, input_sentence_length, input_conversation_length, target_sentences,
-                decode=False):
-        encoder_outputs, encoder_hidden = self.encoder(input_sentences, input_sentence_length)
+    def forward(self, input_utterances, conv_users, input_utterance_length, input_conversation_length,
+                target_utterances, decode=False):
+        num_utterances = input_utterances.size(0)
+        max_conv_len = input_conversation_length.data.max().item()
 
-        context_outputs = torch.cat([encoder_outputs[i, :l, :] for i, l in enumerate(input_conversation_length.data)])
+        _, encoder_hidden = self.encoder(input_utterances, input_utterance_length)
+        encoder_hidden = encoder_hidden.transpose(1, 0).contiguous().view(num_utterances, -1)
+        start = torch.cumsum(torch.cat((to_var(input_conversation_length.data.new(1).zero_()),
+                                        input_conversation_length[:-1])), 0)
+        encoder_hidden = torch.stack([pad(encoder_hidden.narrow(0, s, l), max_conv_len)
+                                      for s, l in zip(start.data.tolist(),
+                                                      input_conversation_length.data.tolist())], 0)
 
-        decoder_init = self.context2decoder(context_outputs)
+        decoder_init = self.context2decoder(encoder_hidden)
         decoder_init = decoder_init.view(self.decoder.num_layers, -1, self.decoder.hidden_size)
 
         if not decode:
-            decoder_outputs = self.decoder(target_sentences, input_users, init_h=decoder_init, decode=decode)
+            decoder_outputs = self.decoder(target_utterances, conv_users, init_h=decoder_init, decode=decode)
             return decoder_outputs
 
         else:
-            prediction, final_score, length = self.decoder.beam_decode(init_h=decoder_init, user_inputs=input_users)
+            prediction, final_score, length = self.decoder.beam_decode(init_h=decoder_init, user_inputs=conv_users)
 
             return prediction
 
-    def generate(self, context, conv_users, sentence_length, n_context):
+    def generate(self, context, conv_users, utterances_length, n_context):
         samples = []
         all_samples = list()
 
         context_outputs = None
         for i in range(n_context):
-            context_outputs, encoder_hidden = self.encoder(context[:, i, :], sentence_length[:, i])
+            context_outputs, encoder_hidden = self.encoder(context[:, i, :], utterances_length[:, i])
 
         decoder_init = self.context2decoder(context_outputs)
         decoder_init = decoder_init.view(self.decoder.num_layers, -1, self.decoder.hidden_size)
